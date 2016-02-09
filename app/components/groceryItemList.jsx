7ee9b6f74adafad00fa6df0adc35bf06ed608aad
pmvfonseca <fonseca.pmvasco@gmail.com>
2016-02-09 20:11:03 +0000

    My first react component

---
+++ app/components/groceryItemList.jsx
@@ -0,0 +1,10 @@
+var React = require('react');
+module.exports = React.createClass({
+    render: function(){
+        return (
+            <div>
+                <h1>Grocery List</h1>
+            </div>
+        );
+    }
+});
--- app/main.jsx
+++ app/main.jsx
@@ -1,1 +1,7 @@
-console.log('here in jsx!!!!');
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+// This works because of browserify
+var GroceryItemList = require('./components/groceryItemList.jsx');
+
+ReactDOM.render(<GroceryItemList />, app);
